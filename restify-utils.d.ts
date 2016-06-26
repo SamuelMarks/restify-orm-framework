218d4dec22daf103fe620a2b025958be37100eff
Samuel Marks <samuelmarks@gmail.com>
2016-06-26 21:45:18 +1000

    Bug fixes

--- restify-utils.d.ts
+++ restify-utils.d.ts
@@ -2,7 +2,7 @@ import * as restify from 'restify';
 import {IModelRoute} from 'nodejs-utils';
 import {Logger} from 'bunyan';
 import * as Waterline from 'waterline';
-import {ConfigOptions} from 'waterline';
+import {ConfigOptions, WLError} from 'waterline';
 import {RedisClient} from 'redis';
 
 declare var restify_utils: restify_utils.restify_utils;
@@ -13,22 +13,24 @@ declare module restify_utils {
     }
 
     export interface IStrapFramework {
-        models_and_routes: IModelRoute,
-        logger: Logger,
-        _cache: {},
-        package_: {version: number},
-        root?: string,
-        start_app?: boolean,
-        skip_db?: boolean,
-        collections?: Waterline.Query[],
-        waterline_config?: ConfigOptions,
-        use_redis?: boolean,
-        redis_cursors?: { redis: RedisClient },
-        createSampleData?: boolean,
-        SampleData?: any, //ISampleData,
-        sampleDataToCreate?: (SampleData: any/*ISampleData*/) => Array<any>,
+        app_name: string;
+        models_and_routes: IModelRoute;
+        logger: Logger;
+        _cache: {};
+        package_: {version: number};
+        root?: string;
+        app_logging?: boolean;
+        start_app?: boolean;
+        skip_db?: boolean;
+        collections?: Waterline.Query[];
+        waterline_config?: ConfigOptions;
+        use_redis?: boolean;
+        redis_cursors?: { redis: RedisClient };
+        createSampleData?: boolean;
+        SampleData?: any; //ISampleData,
+        sampleDataToCreate?: (SampleData: any/*ISampleData*/) => Array<any>;
         //^ <T>(tasks: AsyncFunction<T>[], callback?: AsyncResultArrayCallback<T>) => void,
-        callback?: (app: restify.Server, connections?: any[], collections?: Waterline.Query[]) => void
+        callback?: (err: Error|WLError, app?: restify.Server, connections?: any[], collections?: Waterline.Query[]) => void;
     }
 
     export interface ISampleData {
