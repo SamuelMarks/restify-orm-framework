b81ae5e4c84f1c746a9177459a933be01b165509
Samuel Marks <samuelmarks@gmail.com>
2017-07-10 18:45:30 +1000

    redis=>ioredis. New kwargs: `listen_port`; `redis_config`; and `createServerArgs`. New `onDbInit` callback.

--- index.ts
+++ index.ts
@@ -1,25 +1,28 @@
 import * as restify from 'restify';
+import { bodyParser, queryParser } from 'restify-plugins';
 import * as Waterline from 'waterline';
 import { Collection, waterline, WLError } from 'waterline';
 import { createLogger } from 'bunyan';
-import { WaterlineError } from 'restify-errors';
-import { createClient } from 'redis';
+import { WaterlineError } from 'custom-restify-errors';
+import * as Redis from 'ioredis';
 import { IStrapFramework } from 'restify-utils';
-import { isNullOrUndefined } from 'util';
 
 export const strapFramework = (kwargs: IStrapFramework) => {
-    if (kwargs.root === undefined) kwargs.root = '/api';
-    if (kwargs.app_logging === undefined) kwargs.app_logging = true;
-    if (kwargs.start_app === undefined) kwargs.start_app = true;
-    if (kwargs.skip_db === undefined) kwargs.skip_db = true;
-    if (kwargs.use_redis === undefined) kwargs.use_redis = false;
-    if (kwargs.createSampleData === undefined) kwargs.createSampleData = !process.env['NO_SAMPLE_DATA'];
+    if (kwargs.root == null) kwargs.root = '/api';
+    if (kwargs.app_logging == null) kwargs.app_logging = true;
+    if (kwargs.start_app == null) kwargs.start_app = true;
+    if (kwargs.listen_port == null) /* tslint:disable:no-bitwise */
+        kwargs.listen_port = typeof process.env['PORT'] === 'undefined' ? 3000 : ~~process.env['PORT'];
+    if (kwargs.skip_db == null) kwargs.skip_db = true;
+    if (kwargs.use_redis == null) kwargs.use_redis = false;
+    else if (kwargs.use_redis && kwargs.redis_config == null)
+        kwargs.redis_config = process.env['REDIS_URL'] == null ? { port: 6379 } : process.env['REDIS_URL'];
 
     // Init server obj
-    const app = restify.createServer({ name: kwargs.app_name });
+    const app = restify.createServer(Object.assign({ name: kwargs.app_name }, kwargs.createServerArgs || {}));
 
-    app.use(restify.queryParser());
-    app.use(restify.bodyParser());
+    app.use(queryParser());
+    app.use(bodyParser());
 
     app.on('WLError', (req: restify.Request, res: restify.Response,
                        err: WLError, next: restify.Next) =>
@@ -41,20 +44,20 @@ export const strapFramework = (kwargs: IStrapFramework) => {
         }
     ));
 
+    // Init database obj
+    const waterline_obj: waterline = new Waterline();
+
     const tryTblInit = entity => model =>
         kwargs.models_and_routes[entity].models
         && (kwargs.models_and_routes[entity].models[model].identity
         || kwargs.models_and_routes[entity].models[model].tableName) ?
-            waterline.loadCollection(
+            waterline_obj.loadCollection(
                 Collection.extend(
                     kwargs.models_and_routes[entity].models[model]
                 )
             ) : kwargs.logger.warn(`Not initialising: ${entity}.${model}`);
 
     // models_and_routes['contact'] && tryTblInit('contact')('Contact');
-
-    // Init database obj
-    const waterline: waterline = new Waterline();
 
     Object.keys(kwargs.models_and_routes).map(entity => {
         // Merge routes
@@ -67,10 +70,7 @@ export const strapFramework = (kwargs: IStrapFramework) => {
 
         if (kwargs.models_and_routes[entity].route)
             Object.keys(kwargs.models_and_routes[entity].route).map(
-                route => kwargs.models_and_routes[entity].route[route](
-                    app, `${kwargs.root}/${entity}`
-                )
-            );
+                route => kwargs.models_and_routes[entity].route[route](app, `${kwargs.root}/${entity}`));
 
         // Merge models
         if (!kwargs.skip_db && kwargs.models_and_routes[entity].models)
@@ -78,7 +78,7 @@ export const strapFramework = (kwargs: IStrapFramework) => {
     });
 
     if (kwargs.use_redis) {
-        kwargs.redis_cursors.redis = createClient(process.env['REDIS_URL']);
+        kwargs.redis_cursors.redis = new Redis(kwargs.redis_config);
         kwargs.redis_cursors.redis.on('error', err => {
             kwargs.logger.error(`Redis::error event -
             ${kwargs.redis_cursors.redis['host']}:${kwargs.redis_cursors.redis['port']}s- ${err}`);
@@ -88,27 +88,30 @@ export const strapFramework = (kwargs: IStrapFramework) => {
 
     if (kwargs.skip_db)
         if (kwargs.start_app)
-            app.listen(process.env['PORT'] || 3000, () => {
+            app.listen(kwargs.listen_port, () => {
                 kwargs.logger.info('%s listening at %s', app.name, app.url);
 
-                return kwargs.callback ?
+                return kwargs.callback != null ?
                     kwargs.callback(null, app, Object.freeze([]) as any[], Object.freeze([]) as any[])
                     : null;
             });
-        else if (kwargs.callback)
+        else if (kwargs.callback != null)
             return kwargs.callback(null, app, Object.freeze([]) as any[], Object.freeze([]) as any[]);
 
     // Create/init database models, populated exported collections, serve API
-    waterline.initialize(kwargs.waterline_config, (err, ontology) => {
+    waterline_obj.initialize(kwargs.waterline_config, (err, ontology) => {
         if (err != null) {
-            if (kwargs.callback) return kwargs.callback(err);
+            if (kwargs.callback != null) return kwargs.callback(err);
             throw err;
         }
         /* tslint:disable:one-line */
-        else if (isNullOrUndefined(ontology) || !ontology.connections || !ontology.collections) {
+        else if (
+            ontology == null || ontology.connections == null || ontology.collections == null
+            || ontology.connections.length === 0 || ontology.collections.length === 0
+        ) {
             kwargs.logger.error('ontology =', ontology);
             const error = new TypeError('Expected ontology with connections & collections');
-            if (kwargs.callback) return kwargs.callback(error);
+            if (kwargs.callback != null) return kwargs.callback(error);
             throw error;
         }
 
@@ -118,19 +121,38 @@ export const strapFramework = (kwargs: IStrapFramework) => {
 
         kwargs._cache['collections'] = kwargs.collections; // pass by reference
 
-        if (kwargs.start_app) // Start API server
-            app.listen(process.env['PORT'] || 3000, () => {
-                kwargs.logger.info('%s listening from %s', app.name, app.url);
+        const handleEnd = () => {
+            if (kwargs.start_app) // Start API server
+                app.listen(process.env['PORT'] || 3000, () => {
+                    kwargs.logger.info('%s listening from %s', app.name, app.url);
 
-                if (kwargs.onServerStart) /* tslint:disable:no-empty*/
-                    kwargs.onServerStart(app.url, ontology.connections, kwargs.collections, app,
-                        kwargs.callback == null ? () => {} : kwargs.callback);
-                else if (kwargs.callback)
-                    return kwargs.callback(null, app, ontology.connections, kwargs.collections);
-                return;
-            });
-        else if (kwargs.callback)
-            return kwargs.callback(null, app, ontology.connections, kwargs.collections); // E.g.: for testing
+                    if (kwargs.onServerStart != null) /* tslint:disable:no-empty*/
+                        kwargs.onServerStart(app.url, ontology.connections, kwargs.collections, app,
+                            kwargs.callback == null ? () => {} : kwargs.callback);
+                    else if (kwargs.callback != null)
+                        return kwargs.callback(null, app, ontology.connections, kwargs.collections);
+                    return;
+                });
+            else if (kwargs.callback != null)
+                return kwargs.callback(null, app, ontology.connections, kwargs.collections); // E.g.: for testing
+        };
+
+        if (kwargs.onDbInit) {
+            if (kwargs.onDbInitCb == null)
+                kwargs.onDbInitCb = (error: Error, connections: Waterline.Connection[],
+                                     collections: Waterline.Collection[], finale: () => void): void => {
+                    if (error != null) {
+                        if (kwargs.callback != null) return kwargs.callback(error);
+                        throw error;
+                    }
+                    ontology.connections = connections;
+                    ontology.collections = collections;
+                    return finale();
+                };
+            return kwargs.onDbInit(app, ontology.connections, kwargs.collections, handleEnd, kwargs.onDbInitCb);
+        }
+        else
+            return handleEnd();
     });
 };
 
--- restify-utils.d.ts
+++ restify-utils.d.ts
@@ -1,38 +1,47 @@
 import * as restify from 'restify';
-import { IModelRoute } from 'nodejs-utils';
+import { Server } from 'restify';
 import * as bunyan from 'bunyan';
 import { ConfigOptions, Connection, Query, WLError } from 'waterline';
-import { RedisClient } from 'redis';
-import { Server } from 'restify';
+import { Redis, RedisOptions } from 'ioredis';
 
-declare const restify_utils: restify_utils.restify_utils;
+export type DbInitCb = (err: Error, connections: Connection[], collections: Query[], finale: () => void) => void;
 
-/* tslint:disable:no-namespace no-internal-module */
-declare module restify_utils {
-    /* tslint:disable:interface-name class-name */
-    export interface restify_utils {
-        strapFramework(kwargs: IStrapFramework);
-        add_to_body_mw(...updates: Array<[string, string]>): restify.RequestHandler;
-    }
-
-    export interface IStrapFramework {
-        app_name: string;
-        models_and_routes: IModelRoute;
-        logger: bunyan;
-        _cache: {};
-        package_: {version: number};
-        root?: string;
-        app_logging?: boolean;
-        start_app?: boolean;
-        skip_db?: boolean;
-        collections?: Query[];
-        waterline_config?: ConfigOptions;
-        use_redis?: boolean;
-        redis_cursors?: {redis: RedisClient};
-        onServerStart?: (uri: string, connections: Connection[], collections: Query[], app: Server, next) => void;
-        callback?: (err: Error | WLError, app?: restify.Server,
-                    connections?: Connection[], collections?: Query[]) => void;
-    }
+export interface IStrapFramework {
+    app_name: string;
+    models_and_routes: {
+        [key: string]: {
+            route?: {
+                create?: restify.RequestHandler, read?: restify.RequestHandler,
+                update?: restify.RequestHandler, del?: restify.RequestHandler
+            };
+            routes?: {
+                create?: restify.RequestHandler, read?: restify.RequestHandler,
+                update?: restify.RequestHandler, del?: restify.RequestHandler
+            };
+            models?: any; // ^ Could have more than CRUD, but this is better than `any` or `{}`
+        }
+    };
+    logger: bunyan;
+    _cache: {};
+    package_: {version: number};
+    root?: string;
+    app_logging?: boolean;
+    start_app?: boolean;
+    skip_db?: boolean;
+    collections?: Query[];
+    waterline_config?: ConfigOptions;
+    redis_config?: RedisOptions;
+    use_redis?: boolean;
+    redis_cursors?: {redis: Redis};
+    listen_port?: number;
+    onServerStart?: (uri: string, connections: Connection[], collections: Query[], app: Server, next) => void;
+    onDbInitCb?: DbInitCb;
+    onDbInit?: (app: Server, connections: Connection[], collections: Query[],
+                finale: () => void, next: DbInitCb) => void;
+    createServerArgs?: restify.ServerOptions;
+    callback?: (err: Error | WLError, app?: restify.Server,
+                connections?: Connection[], collections?: Query[]) => void;
 }
 
-export = restify_utils;
+export declare const strapFramework: (kwargs: IStrapFramework) => void;
+export declare const add_to_body_mw: (...updates: Array<[string, string]>) => restify.RequestHandler;
